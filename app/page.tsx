380f9ddf30e3f3e82c943ed131094abe1d7026bf
OxiiLB <romain.giraud@epitech.eu>
2024-09-02 12:24:07 +0000

    [UPD] first test of Bulma Framework for login page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,30 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section class="section">
-        <div class="container has-text-centered">
-          <h1 class="title">Hello World</h1>
-          <p class="subtitle">
-            My first website with
-            <strong class="has-text-primary">Bulma</strong>!
-          </p>
-        </div>
-      </section>
+      <div className="field">
+        <p className="control has-icons-left has-icons-right">
+          <input className="input" type="email" placeholder="Email" />
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope"></i>
+          </span>
+          <span className="icon is-small is-right">
+            <i className="fas fa-check"></i>
+          </span>
+        </p>
+      </div>
+      <div className="field">
+        <p className="control has-icons-left">
+          <input className="input" type="password" placeholder="Password" />
+          <span className="icon is-small is-left">
+            <i className="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div className="field">
+        <p className="control">
+          <button className="button is-success">Login</button>
+        </p>
+      </div>
     </main>
   );
 }
