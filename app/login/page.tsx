2b0adbb80712d354105ebcf06c40110d8f3c6022
matheo2027 <matheo.piques@epitech.eu>
2024-09-03 12:33:33 +0200

    [MDF] Login page end

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,55 +2,46 @@ import styles from "./page.module.css";
 
 export default function Login() {
   return (
-    <main className={styles.LoginPage}>
-      <div className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-offset-3 is-6">
-              <h4 className="is-size-4">Login to your account</h4>
+    <body>
+      <main className={styles.login}>
+        <div className={styles.loginContainer}>
+          <h4 className={styles.title}>Login to your account</h4>
 
-              <form>
-                <div>
-                  <label className="label">E-mail adresse</label>
-                  <div className="control">
-                    <input className="input" type="email" placeholder="Enter your email address"/>
-                  </div>
-                </div>
-
-                <div>
-                  <label className="label">Password</label>
-                  <div className="control">
-                    <input className="input" type="password" placeholder="Enter your Password"/>
-                  </div>
-                </div>
-
-                <div className="level">
-                  <div className="level-left">
-                    <div className="level-item">
-                      <label className="checkbox">
-                        <input type="checkbox"/>
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="level-right">
-                    <div className="level-item">
-                      <button className="button is-text">Forgot your password ?</button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="fiel is-grouped">
-                  <div className="control">
-                    <button className="button is-link">Sign In</button>
-                  </div>
-                </div>
-              </form>
+          <form className={styles.form}>
+            <div className={styles.field}>
+              <label className={styles.label}>E-mail address</label>
+              <input
+                className={styles.input}
+                type="email"
+                placeholder="Enter your email address"
+                required
+              />
             </div>
-          </div>
+
+            <div className={styles.field}>
+              <label className={styles.label}>Password</label>
+              <input
+                className={styles.input}
+                type="password"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+
+            <div className={styles.options}>
+              <label className={styles.checkbox}>
+                <input type="checkbox" />
+                Remember me
+              </label>
+              <button className={styles.buttonText}>Forgot your password?</button>
+            </div>
+
+            <div className={styles.actions}>
+              <button className={styles.buttonLink}>Sign In</button>
+            </div>
+          </form>
         </div>
-      </div>
-    </main>
+      </main>
+    </body>
   );
 }
