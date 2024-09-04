b7a9c3f3fda935d951d77bb2becaf8cd0c82b5fb
Alea Chlodnik <achlodnik@gmail.com>
2024-09-04 12:28:02 +0200

    [UPD] account management page & navbar; [PH] need to make new navbar a reusable component

--- frontend/app/dashboard/account_man/page.tsx
+++ frontend/app/dashboard/account_man/page.tsx
@@ -24,23 +24,35 @@ export default function Home() {
       <main className={styles.main}>
         <nav className="navbar is-fixed-top">
           <div className="navbar-brand">
-            <a className="navbar-item">
+            <p className="navbar-item">
               <strong>Soul Connection</strong>
-            </a>
+            </p>
           </div>
-          <div className="navbar-end">
+          <div className={styles.navbarLinks}>
             <div className="navbar-item">
-              <a className="button">Customers</a>
-              <a className="button">Events</a>
-              <a className="button is-link" onClick={handleDashboardClick}>Dashboard</a>
-              <a className="button is-link" onClick={handleStaticticsClick}>Statictics</a>
-              <a className="button is-link" onClick={handleTipsClick}>Tips</a>
-              <a className="button is-link" onClick={handleAccountsClick}>Accounts</a>
+              <a onClick={handleDashboardClick}>Dashboard</a>
             </div>
+            <div className="navbar-item">
+              <a>Customers</a>
+            </div>
+            <div className="navbar-item">
+              <a>Events</a>
+            </div>
+            <div className="navbar-item">
+              <a onClick={handleTipsClick}>Tips</a>
+            </div>
+            <div className="navbar-item">
+              <a onClick={handleStaticticsClick}>Statictics</a>
+            </div>
+            <div className="navbar-item">
+              <a onClick={handleAccountsClick}>Accounts</a>
+            </div>
+          </div>
+            <div className="navbar-end">
             <div className="navbar-item">
               <img className="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
             </div>
-          </div>
+            </div>
         </nav>
         <div className={styles.heading}>
           <div className={styles.title}>
@@ -48,18 +60,30 @@ export default function Home() {
             <p>You have 87 employees</p> {/* Have it change from employees to coach when filter is applied. Number changes as well */}
           </div>
           <div className={styles.addAccount}>
-            <button className="button is-link is-medium">+</button>
+            <button className="button is-link is-medium">+</button> {/* Add a new employee */}
           </div>
         </div>
         <div className={styles.containerBg}>
           <div className={styles.container}>
-            {/* add filter all, coaches, employees filter buttons (maybe) */}
+            <div className={styles.filter}>
+              <div className="button is-link is-rounded">All</div>
+              <div className="button is-rounded">Coaches</div>
+              <div className="button is-rounded">Admins</div>
+            </div>
             <div className={styles.category}>
               <p>Employee</p> {/* Change to coach when filter is applied */}
               <p>Email</p>
               <p>Phone</p>
               <p>Number of Customers</p>
-              <p>Actions</p> {/* Turn into  */}
+              <p>Actions</p>
+            </div>
+            <div className={styles.employee}>
+              {/* profile picture */}
+              <p><strong>Firstname LastName</strong></p>
+              <p>Email Address</p>
+              <p>Phone Number</p>
+              <p>No.</p>
+              <p>...</p> {/* Turn into button that lets you assign a customer to employee */}
             </div>
           </div>
         </div>
