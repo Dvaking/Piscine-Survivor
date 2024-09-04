3c1f629bd20a21cc65ff898fd632e798e2e05da5
OxiiLB <romain.giraud@epitech.eu>
2024-09-04 08:00:22 +0000

    [ADD] call APi for images

--- backend/API/jobs.ts
+++ backend/API/jobs.ts
@@ -10,30 +10,19 @@ import {
   getCustomers,
   getCustomerById,
   getPaymentsHistory,
+  getCustomerImageById,
+  getClothes,
 } from "./customersApi";
 import {
   getEncounters,
   getEncounterByCustomerId,
   getEncounterById,
 } from "./encountersApi";
-import { getEventById, getEvents } from "./tipsApi";
+import { getEventById, getEvents, getClotheImage } from "./tipsApi";
 
 async function fetchData(): Promise<void> {
   try {
     const token = await login();
-
-    const employees = await getEmployees(token);
-    const customers = await getCustomers(token);
-    const customer = await getCustomerById(token, 1);
-    const employee = await getEmployeeById(token, 1);
-    const employeeMe = await getEmployeeMe(token);
-    const employeeImage = await getEmployeeImageById(token, 1);
-    const paymentsHistory = await getPaymentsHistory(token, 1);
-    const encounters = await getEncounters(token);
-    const encounter = await getEncounterById(token, 1);
-    const encounterByCustomerId = await getEncounterByCustomerId(token, 1);
-    const events = await getEvents(token);
-    const event = await getEventById(token, 1);
 
   } catch (error) {
     console.error("An error occurred while fetching data:", error);
