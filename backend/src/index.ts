7ec51c53f4cb9a3dbdddc1542d91cacf9451d350
OxiiLB <romain.giraud@epitech.eu>
2024-09-04 15:44:43 +0000

    [TO FIX] query to the DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,15 +23,37 @@ import { getEvents, getEventById } from "./API/eventsApi";
 import { getClotheImage } from "./API/clothesApi";
 import { Token, Employee } from "@types";
 
+const { request } = require("graphql-request");
+
+const graphqlURL = "http://localhost:8080/v1/graphql"; // Remplacez par votre URL Hasura
+
+const mutation = `
+  mutation AddEmployee($gender: String, $id: Int, $name: String, $surname: String, $birth_date: String, $work: String) {
+    insert_private_employees(objects: {birth_date: $birth_date, id: $id, gender: $gender, name: $name, surname: $surname, work: $work}) {
+    returning {
+      uuid
+    }
+  }
+`;
+
+const variables = {
+  gender: "M",
+  id: 10,
+  name: "John",
+  surname: "Doe",
+  birth_date: "1990-01-01",
+  work: "Manager",
+};
+
 async function putCustomersInDb(token: Token) {
   const customers = await getCustomers(token);
 
   customers.data.forEach(async (customer) => {
     const customerById = await getCustomerById(token, customer.id);
-    const paymentsHistory = await getPaymentsHistory(token, customer.id);
-    const customerImage = await getCustomerImageById(token, customer.id);
-    const clothes = await getClothes(token, customer.id);
-    const encounters = await getEncounterByCustomerId(token, customer.id);
+    // const paymentsHistory = await getPaymentsHistory(token, customer.id);
+    // const customerImage = await getCustomerImageById(token, customer.id);
+    // const clothes = await getClothes(token, customer.id);
+    // const encounters = await getEncounterByCustomerId(token, customer.id);
   });
 }
 
@@ -55,8 +77,10 @@ async function fetchData(): Promise<void> {
   }
 }
 
-cron.schedule("0 * * * *", () => {
+cron.schedule("* * * * * *", () => {
   fetchData();
+  console.log("-----------------------------------");
+  request(graphqlURL, mutation, variables)
+    .then((data: any) => console.log(data))
+    .catch((error: any) => console.error(error));
 });
-
-fetchData();
