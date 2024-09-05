2127466f76f253b552a8bcd08c1714db6da58684
Alea Chlodnik <achlodnik@gmail.com>
2024-09-05 22:52:58 +0200

    [UPD] data added to astro sign page

--- frontend/app/dashboard/signs/page.tsx
+++ frontend/app/dashboard/signs/page.tsx
@@ -1,61 +1,60 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import { getCustomersInformation } from "@components";
+import { GetCustomersInformationProps } from "@types";
 import styles from "./page.module.css";
 import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from "../../Components/Navbar/Navbar";
 
-// async function fetchCompatibility(sign1: string, sign2: string) {
-//   const userId = '<Your User Id>';
-//   const apiKey = 'e6e70c63639f039518f84a0f3c517837';
-//   const language = 'en';
-//   const data = {
-//     zodiac_sign1: sign1,
-//     zodiac_sign2: sign2
-//   };
-// }
-
-
-
 export default function Home() {
-  const [selectedClient1, setSelectedClient1] = useState<{
-    name: string;
-    sign: string;
-  } | null>(null);
-  const [selectedClient2, setSelectedClient2] = useState<{
-    name: string;
-    sign: string;
-  } | null>(null);
+  const [customers, setCustomers] = useState<GetCustomersInformationProps[]>(
+    []
+  );
+  const [selectedClient1, setSelectedClient1] =
+    useState<GetCustomersInformationProps | null>(null);
+  const [selectedClient2, setSelectedClient2] =
+    useState<GetCustomersInformationProps | null>(null);
   const [dropdownForClient1, setDropdownForClient1] = useState(false);
   const [dropdownForClient2, setDropdownForClient2] = useState(false);
-  const clients = [
-    { name: "Diana White", sign: "Aries" },
-    { name: "Charlie Brown", sign: "Taurus" },
-  ];
+  const [result, setResult] = useState<number | null>(null);
+
+  // Fetch customers data
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedCustomers = await getCustomersInformation();
+      setCustomers(fetchedCustomers);
+    };
+    fetchData();
+  }, []);
+
   const handleClient1Click = () => {
     setDropdownForClient1(!dropdownForClient1);
     setDropdownForClient2(false);
   };
+
   const handleClient2Click = () => {
     setDropdownForClient2(!dropdownForClient2);
     setDropdownForClient1(false);
   };
-  const selectClient1 = (client: { name: string; sign: string }) => {
+
+  const selectClient1 = (client: GetCustomersInformationProps) => {
     setSelectedClient1(client);
     setDropdownForClient1(false);
   };
-  const selectClient2 = (client: { name: string; sign: string }) => {
+
+  const selectClient2 = (client: GetCustomersInformationProps) => {
     setSelectedClient2(client);
     setDropdownForClient2(false);
   };
 
-  const [result, setResult] = useState<number | null>(null);
+  // Handle button click for generating a random percentage
   const handleButtonClick = () => {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     setResult(randomNumber);
   };
+
   const isButtonDisabled = !selectedClient1 || !selectedClient2;
 
   return (
@@ -64,7 +63,7 @@ export default function Home() {
       <div className={styles.heading}>
         <div className={styles.title}>
           <h1>
-            Astrological Sign Compatibility<i className="far fa-star"></i>
+            Astrological Sign Compatibility <i className="far fa-star"></i>
           </h1>
           <p>Check if you're a match</p>
         </div>
@@ -86,11 +85,12 @@ export default function Home() {
                     <img
                       className="is-rounded"
                       src="https://bulma.io/assets/images/placeholders/128x128.png"
+                      alt="Client"
                     />
                   </figure>
                   <div className={styles.clientInfo}>
                     <h2>{selectedClient1.name}</h2>
-                    <p>{selectedClient1.sign}</p>
+                    <p>{selectedClient1.astrological_sign}</p>
                   </div>
                 </>
               ) : (
@@ -100,29 +100,30 @@ export default function Home() {
             {dropdownForClient1 && (
               <div className={styles.dropdown}>
                 <ul>
-                  {clients
-                    .filter((client) => client.name !== selectedClient2?.name)
-                    .map((client, index) => (
+                  {customers
+                    .filter((client) => client.id !== selectedClient2?.id)
+                    .map((client) => (
                       <li
-                        key={index}
+                        key={client.id}
                         className={styles.listItem}
                         onClick={() => selectClient1(client)}
                       >
-                        {client.name}
+                        {client.name} ({client.astrological_sign})
                       </li>
                     ))}
                 </ul>
               </div>
             )}
-
-            <div className={
-                result
-                  ? styles.resultsBoxActive
-                  : styles.resultsBoxInactive
-              }>
+            <div
+              className={
+                result ? styles.resultsBoxActive : styles.resultsBoxInactive
+              }
+            >
               {result !== null ? (
                 <>
-                  <p><strong>{result}%</strong></p>
+                  <p>
+                    <strong>{result}%</strong>
+                  </p>
                   <p>Compatible</p>
                 </>
               ) : (
@@ -133,7 +134,6 @@ export default function Home() {
                 </p>
               )}
             </div>
-
             <div
               className={
                 selectedClient2
@@ -148,11 +148,12 @@ export default function Home() {
                     <img
                       className="is-rounded"
                       src="https://bulma.io/assets/images/placeholders/128x128.png"
+                      alt="Client"
                     />
                   </figure>
                   <div className={styles.clientInfo}>
                     <h2>{selectedClient2.name}</h2>
-                    <p>{selectedClient2.sign}</p>
+                    <p>{selectedClient2.astrological_sign}</p>
                   </div>
                 </>
               ) : (
@@ -162,15 +163,15 @@ export default function Home() {
             {dropdownForClient2 && (
               <div className={styles.dropdown}>
                 <ul>
-                  {clients
-                    .filter((client) => client.name !== selectedClient1?.name)
-                    .map((client, index) => (
+                  {customers
+                    .filter((client) => client.id !== selectedClient1?.id)
+                    .map((client) => (
                       <li
-                        key={index}
+                        key={client.id}
                         className={styles.listItem}
                         onClick={() => selectClient2(client)}
                       >
-                        {client.name}
+                        {client.name} ({client.astrological_sign})
                       </li>
                     ))}
                 </ul>
