b3f4e658c5cfc96f265ded84a692baa4a991ca96
matheo2027 <matheo.piques@epitech.eu>
2024-09-06 02:19:46 +0200

    [UPD] GetClothes =! GetEmployees

--- frontend/app/Components/Employees/Get.ts
+++ frontend/app/Components/Employees/Get.ts
@@ -2,22 +2,16 @@ import {
   client,
   GetEmployeesInformation,
   GetEmployeesInformationByUuid,
-  GetClothesByCustomerEMail,
 } from "@queries";
 import {
   GetEmployeesInformationProps,
   GetEmployeesInformationByUuidProps,
-  GetClothesProps,
 } from "@types";
 
 export interface Employees {
   private_employees:
     | GetEmployeesInformationProps[]
     | GetEmployeesInformationByUuidProps[];
-}
-
-export interface Clothes {
-  private_customers: GetClothesProps[];
 }
 
 export async function getEmployeesInformation() {
@@ -38,17 +32,4 @@ export async function getEmployeesInformationByUuid() {
     console.error("Erreur lors de l'insertion:", error);
   }
   return response ? response.private_employees : [];
-}
-
-export async function getClothesByCustomerEMail(email: string) {
-  let arg = {
-    email: email,
-  }
-  let response: Clothes | undefined = undefined;
-  try {
-    response = await client.request(GetClothesByCustomerEMail, arg);
-  } catch (error) {
-    console.error("Erreur lors de l'insertion:", error);
-  }
-  return response ? response.private_customers : [];
 }
