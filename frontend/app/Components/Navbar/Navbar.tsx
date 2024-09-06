c48d9af1698af02c2bfd608b67b176168beaa55e
Alea Chlodnik <achlodnik@gmail.com>
2024-09-06 14:28:43 +0200

    [ADD] re-adding navbar modifs

--- frontend/app/Components/Navbar/Navbar.tsx
+++ frontend/app/Components/Navbar/Navbar.tsx
@@ -8,19 +8,29 @@ import "bulma/css/bulma.css";
 export default function Navbar() {
   const router = useRouter();
 
-  const handleClick = (categories: string) => {
-    if (categories === "clothes") router.push("../../dashboard/clothes");
-    else if (categories === "customers")
-      router.push("../../dashboard/customers");
-    else if (categories === "statistics")
-      router.push("../../dashboard/statistics");
-    else if (categories === "coaches")
-      router.push("../../dashboard/account_man");
-    else if (categories === "client_profile")
-      router.push("../../dashboard/client_profile");
-    else if (categories === "tips") router.push("../../dashboard/tips");
-    else if (categories === "signs") router.push("../../dashboard/signs");
-    else if (categories === "dashboard") router.push("../../dashboard");
+  const handleClothesClick = () => {
+    router.push("../../dashboard/clothes");
+  };
+  const handleCustomersClick = () => {
+    router.push("../../dashboard/customers");
+  };
+  const handleTipsClick = () => {
+    router.push("../../dashboard/tips");
+  };
+  const handleStaticticsClick = () => {
+    router.push("../../dashboard/statistics");
+  };
+  const handleAccountsClick = () => {
+    router.push("../../dashboard/account_man");
+  };
+  const handleClientProfileClick = () => {
+    router.push("../../dashboard/client_profile");
+  };
+  const handleSignsClick = () => {
+    router.push("../../dashboard/signs");
+  };
+  const handleDashboardClick = () => {
+    router.push("../../dashboard");
   };
 
   return (
@@ -35,39 +45,43 @@ export default function Navbar() {
         </div>
         <div className={styles.navbarLinks}>
           <div className="navbar-item">
-            <a onClick={() => handleClick("dashboard")}>Dashboard</a>
+            <a onClick={handleClientProfileClick}>Client Profile</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("coaches")}>Coaches</a>
+            <a onClick={handleDashboardClick}>Dashboard</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("customers")}>Customers</a>
+            <a onClick={handleCustomersClick}>Customers</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("tips")}>Tips</a>
+            <a onClick={handleTipsClick}>Tips</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("client_profile")}>Client Profile</a>
-          </div>
-          <div className="navbar-item">
-            <a onClick={() => handleClick("statistics")}>Statistics</a>
+            <a onClick={handleStaticticsClick}>Statistics</a>
           </div>
           <div className="navbar-item">
             <a>Events</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("clothes")}>Clothes</a>
+            <a onClick={handleAccountsClick}>Accounts</a>
           </div>
           <div className="navbar-item">
-            <a onClick={() => handleClick("signs")}>Signs</a>
+            <a onClick={handleClothesClick}>Clothes</a>
           </div>
           <div className="navbar-item">
+            <a onClick={handleSignsClick}>Signs</a>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className={styles.profilePic}>
+            <div className="navbar-item">
             <img
               className="is-rounded"
               src="https://bulma.io/assets/images/placeholders/128x128.png"
             />
             </div>
           </div>
+        </div>
       </nav>
     </div>
   );
