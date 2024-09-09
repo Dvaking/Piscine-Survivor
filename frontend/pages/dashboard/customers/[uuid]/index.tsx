109494d8c63c5e65fe31544fc728ee1a2402b1c9
Marc <marc.mendia@epitech.eu>
2024-09-09 17:30:43 +0200

    [TMP}

--- frontend/pages/dashboard/customers/[uuid]/index.tsx
+++ frontend/pages/dashboard/customers/[uuid]/index.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD:frontend/app/dashboard/customers/page.tsx
-"use client";
-
 import { useEffect, useRef } from "react";
-import { Navbar } from "@components";
 import React, { useState } from "react";
-import styles from "./page.module.css";
+import styles from "@styles/CustomersPage.module.css";
 import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -59,112 +55,6 @@ export default function Home() {
   };
 
   return (
-    <main className={styles.main}>
-      <Navbar />
-      <div className={styles.heading}>
-        <div className={styles.title}>
-          <h1>Customers List</h1>
-          <p>You have 87 employees</p>
-        </div>
-        <div className={styles.exportAddButtons}>
-          <div>
-            <button className="button is-medium">Export</button>
-          </div>
-          <div>
-            <button className="button is-link is-large" onClick={togglePopup}>
-            <i className="fas fa-plus" aria-hidden="true"></i>
-            </button>
-          </div>
-        </div>
-=======
-import { useRouter } from "next/navigation";
-import styles from "@styles/CustomersPage.module.css";
-import "bulma/css/bulma.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-export default function Customers() {
-  const router = useRouter();
-
-  return (
-    <div className={`${styles.container} mt-6`}>
-      <div className={styles.headerContainer}>
-        <h1 className={styles.title}>Customers Details</h1>
-        <button className={styles.backButton} onClick={() => router.back()}>
-          <i className="fas fa-arrow-left"></i> Back
-        </button>
->>>>>>> b9d18f75748cc1dac7724191fe1834254e71d143:frontend/pages/dashboard/customers/[uuid]/index.tsx
-      </div>
-
-      {isPopupVisible && (
-        <div className={styles.popupOverlay} onClick={togglePopup}>
-          <div
-            className={styles.popupContent}
-            onClick={(e) => e.stopPropagation()}
-          >
-            <h2>New Employee</h2>
-            <form>
-              <div className="field">
-                <label className="label">Name</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Enter your full name"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="email"
-                    placeholder="Enter your email address"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label">Phone</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="phone"
-                    placeholder="Enter your phone number"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label">Job</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="job"
-                    placeholder="Enter your job type (ex: coach)"
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <div className={styles.formButtons}>
-                  <div>
-                    <button className="button is-link" type="submit">
-                      Save
-                    </button>
-                  </div>
-                  <div>
-                    <button
-                      className="button"
-                      type="button"
-                      onClick={togglePopup}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
 
       <div className={styles.containerBg}>
         <div className={styles.container}>
@@ -201,49 +91,6 @@ export default function Customers() {
               <i className="fas fa-cog" aria-hidden="true"></i>
             </div>
           </div>
-<<<<<<< HEAD:frontend/app/dashboard/customers/page.tsx
-          <div className={styles.category}>
-            <div className={styles.checkName}>
-              <i className="far fa-square"></i>
-              Employee
-            </div>
-            <div>Email</div>
-            <div>Phone</div>
-            <div>Number of Customers</div>
-            <div>
-              <p className={styles.actions}>Actions</p>
-            </div>
-          </div>
-          <div className={styles.employee}>
-            <div className={styles.checkName}>
-              <i className="far fa-square"></i>
-              <p>
-                <strong>Firstname LastName</strong>
-              </p>
-            </div>
-            <div>Email Address</div>
-            <div>Phone Number</div>
-            <div>No.</div>
-            <div className={styles.addClientButton}>
-              <button className={styles.actions} onClick={toggleDropdown}>
-                <i className="fas fa-ellipsis-h"></i>
-              </button>
-              {isDropdownOpen && (
-            <div className={styles.dropdown}>
-              <ul>
-                {names.map((name, index) => (
-                  <li
-                    key={index}
-                    onClick={() => handleNameClick(name)}
-                    className={styles.listItem}
-                  >
-                    {name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-=======
 
           <div className="column">
             <div className="box">
@@ -368,7 +215,6 @@ export default function Customers() {
                   </tbody>
                 </table>
               </div>
->>>>>>> b9d18f75748cc1dac7724191fe1834254e71d143:frontend/pages/dashboard/customers/[uuid]/index.tsx
             </div>
           </div>
         </div>
