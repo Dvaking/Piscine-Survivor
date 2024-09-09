6d2d0a5dd233167f1f2f91ab50f14549197808c3
Alea Chlodnik <achlodnik@gmail.com>
2024-09-09 08:57:32 +0200

    [MERGE]

--- frontend/queries/clothes.ts
+++ frontend/queries/clothes.ts
@@ -4,6 +4,7 @@ export const GetClothesByCustomerEMail = gql`
   query GetClothesByCustomerEMail($email: String) {
     private_customers(where: { email: { _eq: $email } }) {
       clothes {
+        id
         image
         type
         id
