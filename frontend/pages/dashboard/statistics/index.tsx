aa66e9f59b7778af478c4ce73fbb23e7de575823
matheo2027 <matheo.piques@epitech.eu>
2024-09-10 15:39:38 +0200

    [UPD] Statistics okay

--- frontend/pages/dashboard/statistics/index.tsx
+++ frontend/pages/dashboard/statistics/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Line, Pie } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -12,7 +12,7 @@ import {
   ArcElement,
 } from "chart.js";
 import styles from "@styles/StatisticsPage.module.css";
-import { getEmployeesByWork, getEmployeesAssignedCustomers } from "@hooks"; // Ajout de la nouvelle fonction
+import { getEmployeesByWork, getEmployeesAssignedCustomers } from "@hooks";
 import {
   GetEmployeesByWorkProps,
   GetEmployeesAssignedCustomersProps,
@@ -31,71 +31,51 @@ ChartJS.register(
   ArcElement
 );
 
-const StaticticsGraph: React.FC = () => {
-  const [employeesData, setEmployeesData] = useState<GetEmployeesByWorkProps[]>(
-    []
-  );
-  const [assignedCustomersData, setAssignedCustomersData] = useState<
-    GetEmployeesAssignedCustomersProps[]
-  >([]); // Nouvel état pour les clients assignés
+const StatisticsGraph: React.FC = () => {
+  const [employeesData, setEmployeesData] = useState<GetEmployeesByWorkProps[]>([]);
+  const [assignedCustomersData, setAssignedCustomersData] = useState<GetEmployeesAssignedCustomersProps[]>([]);
+
   const chartRef = useRef(null);
   const pieChartRef = useRef(null);
 
-  const fetchEmployeesData = async () => {
-    try {
-      const data = await getEmployeesByWork();
-      const mappedData: GetEmployeesByWorkProps[] = data.map((item) => ({
-        name: item.name,
-        image:
-          "image" in item && typeof item.image === "string"
-            ? item.image
-            : undefined,
-        email:
-          "email" in item && typeof item.email === "string"
-            ? item.email
-            : undefined,
-        events:
-          "events" in item && Array.isArray(item.events) ? item.events : [],
-      }));
-
-      setEmployeesData(mappedData);
-    } catch (error) {
-      console.error("Erreur lors de la récupération des données", error);
-    }
-  };
-
-  const fetchAssignedCustomersData = async () => {
-    try {
-      const data = await getEmployeesAssignedCustomers();
-
-      console.log("Données des clients assignés : ", data);
-
-      const mappedCustomersData: GetEmployeesAssignedCustomersProps[] =
-        data.map((item) => ({
-          name: item.name,
-          customers_assign: item.customers_assign || [],
-        }));
-
-      setAssignedCustomersData(mappedCustomersData);
-    } catch (error) {
-      console.error(
-        "Erreur lors de la récupération des données des clients assignés",
-        error
-      );
-    }
-  };
-
   useEffect(() => {
-    fetchEmployeesData();
-    fetchAssignedCustomersData();
+    (async () => {
+      try {
+        const data = await getEmployeesByWork();
+        const mappedData = data.map((item: any) => ({
+          name: item.name,
+          image: typeof item.image === "string" ? item.image : undefined,
+          email: typeof item.email === "string" ? item.email : undefined,
+          events: Array.isArray(item.events) ? item.events : [],
+        }));
+        console.log("MAPPEDDATA", mappedData);
+        setEmployeesData(mappedData);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données des employés :", error);
+      }
+    })();
+
+    (async () => {
+      try {
+        const data = await getEmployeesAssignedCustomers();
+        const mappedCustomersData = data.map((item: any) => ({
+          name: item.name,
+          customers_assign: item.customer_asing || [],
+        }));
+        console.log("mappedCustomersData", mappedCustomersData);
+        setAssignedCustomersData(mappedCustomersData);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données des clients assignés :", error);
+      }
+    })();
   }, []);
 
   const lineChartData = {
     labels: employeesData.map((employee) => employee.name),
     datasets: [
       {
-        label: "Number of Events",
-        data: employeesData.map((employee) => employee.events?.length ?? 0),
+        label: "Nombre d'événements",
+        data: employeesData.map((employee) => employee.events?.length || 0),
         fill: false,
         backgroundColor: "rgb(75, 192, 192)",
         borderColor: "rgba(75, 192, 192, 0.2)",
@@ -118,17 +98,15 @@ const StaticticsGraph: React.FC = () => {
     labels: assignedCustomersData.map((employee) => employee.name),
     datasets: [
       {
-        label: "Customers Distribution",
-        data: assignedCustomersData.map(
-          (employee) => employee.customers_assign?.length ?? 0
-        ),
+        label: "Répartition des clients",
+        data: assignedCustomersData.map((employee) => employee.customers_assign.length || 0),
         backgroundColor: backgroundColors,
         hoverOffset: 4,
       },
     ],
   };
 
-  const options = {
+  const chartOptions = {
     responsive: true,
     plugins: {
       legend: {
@@ -136,7 +114,7 @@ const StaticticsGraph: React.FC = () => {
       },
       title: {
         display: true,
-        text: "Coaches Statistics",
+        text: "Statistiques des employés",
       },
     },
   };
@@ -147,11 +125,8 @@ const StaticticsGraph: React.FC = () => {
         <div className="column is-half-desktop is-full-mobile">
           <div className="box is-shadowless has-background-white mb-6 ml-4 mr-4 mt-4">
             <div className="field">
-              <div
-                className={styles["chart-container"]}
-                style={{ height: "37rem" }}
-              >
-                <Line ref={chartRef} data={lineChartData} options={options} />
+              <div className={styles["chart-container"]} style={{ height: "37rem" }}>
+                <Line ref={chartRef} data={lineChartData} options={chartOptions} />
               </div>
             </div>
           </div>
@@ -159,11 +134,8 @@ const StaticticsGraph: React.FC = () => {
         <div className="column is-half-desktop is-full-mobile">
           <div className="box is-shadowless has-background-white mb-6 ml-4 mr-4 mt-4">
             <div className="field">
-              <div
-                className={styles["chart-container"]}
-                style={{ height: "37rem" }}
-              >
-                <Pie ref={pieChartRef} data={pieChartData} options={options} />
+              <div className={styles["chart-container"]} style={{ height: "37rem" }}>
+                <Pie ref={pieChartRef} data={pieChartData} options={chartOptions} />
               </div>
             </div>
           </div>
@@ -173,4 +145,4 @@ const StaticticsGraph: React.FC = () => {
   );
 };
 
-export default StaticticsGraph;
+export default StatisticsGraph;
